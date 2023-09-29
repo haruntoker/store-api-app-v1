20d8638ea58af982921347323e351315b95bd555
Harun <haruntoker@live.com>
2023-09-29 12:10:02 +0300

    find, sort etc methods added and tested

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,48 @@
+const Product = require('../models/product')
 
 
 //1
 const getAllProductsStatic = async (req,res) => {
-    throw new Error("testing async error")
-    res.status(200).json({msg: 'products testing route'})
+    const products = await Product.find({}).sort('-name price')
+    res.status(200).json({products, nbHits: products.length})
 }
 
 //2
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg: 'products  route'})
-}
+    const {featured, company, name, sort} = req.query
+    const queryObject = {}
+    //A
+    if(featured){
+        queryObject.featured = featured === "true" ? true : false
+    }
+    // B
+    if(company){
+        queryObject.company = company 
+    }
+    //C
+    if(name){
+        queryObject.name = { $regex: name, $options: "i"}
+    }
+
+    console.log(queryObject);
+
+    let result =  Product.find(queryObject);
+
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    }else{
+        result = result.sort("createdAt")
+    }
+    
+    const products = await result
+    res.status(200).json({products, nbHits: products.length})
+};
 
 
+
+
+//export
 module.exports = {
     getAllProductsStatic,
     getAllProducts
